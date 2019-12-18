95645b7009d07a69cbbca08a3301fd22fbdc8718
kumawatajay <48020923+kumawatajay@users.noreply.github.com>
2019-12-18 15:36:35 +0530

    Code updated

--- main_master/codex.py
+++ main_master/codex.py
@@ -8,14 +8,13 @@ params = json.loads(open('/home/pi/configuration.json').read())
 sys.stdout = open(params['Codex_Log_File'],'a')     #Creating all the logs in log_files directory
 
 
-#checkfiles() function checks if there is any file present in the slaves (rasp1/rasp2/rasp3) folder , if there is any file it runs
-# python file code1.py which basically sends the data to main master and deletes the files(images) from the folder
+
 def fun():
     #subprocess.Popen("ssh {user}@{host} {cmd}".format(user="pi",host="192.168.43.112",cmd="scp rasp1/idata/*.png pi@192.168.43.113:master/rasp1/idata"), shell=True, stdout=subprocess.PIPE).communicate()
     subprocess.Popen("{cmd}".format(cmd="mkdir  -p /mnt/sda1"), shell=True, stdout=subprocess.PIPE).communicate()
 
     subprocess.Popen("{cmd} {usbpath} {mountpath}".format(cmd="sudo mount -o rw", usbpath=params['usblocation'],mountpath=params['destination']), shell=True, stdout=subprocess.PIPE).communicate()
-    #subprocess.Popen("{cmd} {mainmaster} {mountpath}".format(cmd="sudo cp -r",mainmaster=params['main_master'],mountpath=params['destination']), shell=True, stdout=subprocess.PIPE).communicate()
+    subprocess.Popen("{cmd} {mainmaster} {mountpath}".format(cmd="sudo cp -r",mainmaster=params['main_master'],mountpath=params['destination']), shell=True, stdout=subprocess.PIPE).communicate()
 
 def runCode1():
 	res = subprocess.Popen("{cmd}".format(cmd="python code1.py"), shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
